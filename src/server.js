15372fe49d498d3883dcb1a05040f0b7c2078dbb
fagnerdev.souza <fagner.cicero@gmail.com>
2020-06-05 11:14:00 -0300

    adicao do nunjucks

--- src/server.js
+++ src/server.js
@@ -4,10 +4,23 @@ const server = express();
 // configurar pasta pública
 server.use(express.static("public"))
 
+//configurar nunjucks
+const nunjucks = require('nunjucks')
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
 
 server.get('/', (req, res) => {
-
-    console.log("No server")
-    res.sendFile(__dirname + "/views/index.html")
+    console.log("Server: No index")
+    return res.render("index.html")
+})
+server.get('/cadastroPonto', (req, res) => {
+    console.log("Serve: No cadastro")
+    return res.render("cadastro.html")
+})
+server.get('/pontoColeta', (req, res) => {
+    console.log("Server: Na lista de pontos")
+    return res.render("listaPonto.html")
 })
 server.listen(3000);
